f07ae7ceb6757b9d146eadc20024f665bae197f1
Thanga Ganapathy <ganapathy888@gmail.com>
2022-03-24 13:07:54 +0530

    Docs: Add github icon to header

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -43,7 +43,8 @@ const config = {
         items: [
           {
             href: 'https://github.com/open-tech-world/react-state',
-            label: 'GitHub',
+            // label: 'GitHub',
+            className: 'header-github-link',
             position: 'right',
           },
         ],
