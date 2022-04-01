1feab1ead51ede27fd2ff14a9a92c12acce3608f
Thanga Ganapathy <ganapathy888@gmail.com>
2022-04-01 15:56:28 +0530

    Add basic example

---
+++ examples/Basic/App.js
@@ -0,0 +1,35 @@
+import { createState } from '@open-tech-world/react-state';
+
+import './styles.css';
+
+const useCounterState = createState({ counter: 0 });
+
+const Counter = () => {
+  const [counter, setCounterState] = useCounterState((s) => s.counter, {
+    set: true
+  });
+  return (
+    <div className="counter">
+      <button
+        onClick={() => setCounterState((s) => ({ counter: s.counter - 1 }))}
+      >
+        -
+      </button>
+      <div className="counter-value">{counter}</div>
+      <button
+        onClick={() => setCounterState((s) => ({ counter: s.counter + 1 }))}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>COUNTER</h1>
+      <Counter />
+    </div>
+  );
+}
